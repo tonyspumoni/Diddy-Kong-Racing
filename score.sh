7a04c95139a28eff604de90fc40db21fc5b41a20
Mike Grimes <magrimes+new_github@mtu.edu>
2022-10-20 18:17:13 -0600

    add score updater thingy

--- score.sh
+++ score.sh
@@ -1,1 +1,18 @@
-python3 ./tools/python/score.py "$@"
+if [[ $1 == '--update-readme' ]]; then
+  export NEW_SCORE=$(python3 ./tools/python/score.py "$@")
+  echo $NEW_SCORE
+  python3 - <<'EOF'
+import sys
+import os
+import re
+print(
+  re.sub(
+    r'(?s).<!-- README_SCORE_BEGIN -->.*<!-- README_SCORE_END -->',
+    f'\n<!-- README_SCORE_BEGIN-->\n{os.environ["NEW_SCORE"]}\n<!-- README_SCORE_END -->',
+    open('README.md', 'r').read()
+  )
+)
+EOF
+else
+  python3 ./tools/python/score.py "$@"
+fi
